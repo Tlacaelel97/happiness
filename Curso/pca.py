04d948f5f819335bf5f937f95454269b0b69b845
Tlacaelel <tlacafv@gmail.com>
2021-06-06 21:50:14 -0500

    PCA comment

--- Curso/pca.py
+++ Curso/pca.py
@@ -1,3 +1,4 @@
+#PCA delimita cuales son los features que mas peso tienen en el modelos
 from math import log
 import pandas as pd
 import sklearn 
@@ -17,13 +18,15 @@ if __name__ == "__main__":
 
     #quita la columna target
     dt_features = dt_heart.drop(['target'], axis=1)
+    #crea la target
     dt_target = dt_heart['target']
 
     #normalizar los datos
     dt_features = StandardScaler().fit_transform(dt_features)
 
-    #split de los datos
+    #split de los datos, random state lo dejamps como un numero fijo para repetibilidad
     X_train, X_test, y_train, y_test = train_test_split(dt_features, dt_target, test_size=0.3, random_state=42)
+
 
     print(X_train.shape)
     print(y_train.shape)
@@ -45,6 +48,7 @@ if __name__ == "__main__":
     #aplicar pca
     dt_train = pca.transform(X_train)#aplicacion
     dt_test = pca.transform(X_test)
+    #ajustamos el modelo
     logistic.fit(dt_train,y_train)
     print("SCORE PCA: ", logistic.score(dt_test, y_test)) #conjuntode prueba contra los datos preparados para la prediccion
 
